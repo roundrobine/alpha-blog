86993e9374be5e906b1648aa09a00b3179cd30c2
roundrobine <roundrobine@gmail.com>
2016-12-08 00:47:26 +0200

    Add admin user functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -35,7 +35,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-
   
     @article.destroy
 
@@ -83,7 +82,7 @@ class ArticlesController < ApplicationController
 
    def require_same_user
 
-    if current_user != @article.user
+    if current_user != @article.user and !current_user.admin?
       flash[:danger] = 'You can only edit or delete your own articles!'
       redirect_to root_path
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only:[:edit, :update, :show]
-  before_action :require_same_user, only:[:edit, :update]
+  before_action :set_user, only:[:edit, :update, :show, :destroy]
+  before_action :require_same_user, only:[:edit, :update, :destroy]
+  before_action :require_admin, only:[:destroy]
 
 
   def index
@@ -42,12 +43,18 @@ class UsersController < ApplicationController
   end
 
   def show
-
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 3)
-
-
   end
 
+ def destroy
+  
+    @user.destroy
+
+    flash[:success] = "User and all articles created by the user was sucessfuly deleted"
+
+    redirect_to users_path
+
+  end
 
 
   private
@@ -63,11 +70,19 @@ class UsersController < ApplicationController
 
    def require_same_user
 
-    if current_user != @user
+    if current_user != @user and !current_user.admin?
       flash[:danger] = 'You can only edit your own user profile!'
       redirect_to root_path
     end
    end
+
+   def require_admin
+    if logged_in? and !current_user.admin? 
+      flash[:danger] = 'Only admin users can perform that action!'
+      redirect_to root_path
+    end
+   end
+
 
 
 end
